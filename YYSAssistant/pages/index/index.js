d6fa961073a286e3ca94189d42608084f4435545
dean.wu <dean.wu@deanwudeMac-mini.local>
2019-01-21 20:12:08 +0800

    [斗技助手]微信小程序,初步完成UI

---
+++ YYSAssistant/pages/index/index.js
@@ -0,0 +1,33 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+
+  },
+  onLoad: function () {
+    var SSList = [
+      {
+        img:"../../images",
+        name: "式神1",
+      },
+      {
+        img: "../../images",
+        name: "式神2",
+      },
+      {
+        img: "../../images",
+        name: "式神3",
+      },
+      {
+        img: "../../images",
+        name: "式神4",
+      },
+      {
+        img: "../../images",
+        name: "式神5",
+      }
+    ];
+  }
+})
